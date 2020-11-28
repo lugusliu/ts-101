42f4733091659e0eec8f436a6aa07e334b358d42
lugusliu <lugusliu@qq.com>
2020-11-28 21:05:36 +0800

    add enum

--- ts-base/src/datatype.ts
+++ ts-base/src/datatype.ts
@@ -1,43 +1,43 @@
 // 原始类型
-let bool: boolean = true
-let num: number = 123
-let str: string | null = 'abc'
+let bool: boolean = true;
+let num: number = 123;
+let str: string | null = "abc";
 
 // 数组
-let arr1: number[] = [1, 2, 3]
-let arr2: Array<number | string> = [1, 2, 3, '4']
+let arr1: number[] = [1, 2, 3];
+let arr2: Array<number | string> = [1, 2, 3, "4"];
 
 // 元组
-let tuple: [number, string, boolean] = [1, '2', true]
+let tuple: [number, string, boolean] = [1, "2", true];
 
 // 函数
-let add = (x: number, y: number) => x + y
-let compute: (x: number, y: number) => number
-compute = (a, b) => a + b
+let add = (x: number, y: number) => x + y;
+let compute: (x: number, y: number) => number;
+compute = (a, b) => a + b;
 
 // 对象
-let obj: { x: number, y: number } = { x: 1, y: 2 }
-obj.x = 3
+let obj: { x: number; y: number } = { x: 1, y: 2 };
+obj.x = 3;
 
 // symbol
-let s1: symbol = Symbol()
-let s2 = Symbol()
+let s1: symbol = Symbol();
+let s2 = Symbol();
 
 // undefined, null
-let un: undefined = undefined
-let nu: null = null
-str = null
+let un: undefined = undefined;
+let nu: null = null;
+str = null;
 
 // void
-let noReturn = () => { }
+let noReturn = () => {};
 
 // any
-let x
+let x;
 
 // never
 let error = () => {
-  throw new Error('error')
-}
+  throw new Error("error");
+};
 let endless = () => {
-  while(true) {}
-}
+  while (true) {}
+};
--- ts-base/src/index.ts
+++ ts-base/src/index.ts
@@ -1,5 +1,5 @@
+import "./datatype";
+import "./enum";
 
-import './datatype'
-
-let hello: string = 'hello Typescript'
-document.querySelectorAll(".app")[0].textContent = hello
+let hello: string = "hello Typescript";
+document.querySelectorAll(".app")[0].textContent = hello;
