f3b2ff174cf2be3dc0b6560dab3e5ada9a2c2890
lugusliu <lugusliu@qq.com>
2020-11-30 23:28:35 +0800

    add generics

--- ts-base/src/index.ts
+++ ts-base/src/index.ts
@@ -4,6 +4,7 @@ import "./interface";
 import "./function";
 import "./class";
 import "./class-interface";
+import "./generics";
 
 let hello: string = "hello Typescript";
 document.querySelectorAll(".app")[0].textContent = hello;
