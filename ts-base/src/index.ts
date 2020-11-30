b769a9a27fdfa6b44647a11ddcec3215ba074f09
lugusliu <lugusliu@qq.com>
2020-11-30 23:32:05 +0800

    调整目录结构

--- ts-base/src/index.ts
+++ ts-base/src/index.ts
@@ -1,10 +1,10 @@
-import "./datatype";
-import "./enum";
-import "./interface";
-import "./function";
-import "./class";
-import "./class-interface";
-import "./generics";
+import "./base/datatype";
+import "./base/enum";
+import "./base/interface";
+import "./base/function";
+import "./base/class";
+import "./base/class-interface";
+import "./base/generics";
 
 let hello: string = "hello Typescript";
 document.querySelectorAll(".app")[0].textContent = hello;
